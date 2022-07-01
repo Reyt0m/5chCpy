5651e6d1a434643bbe444400e6c4297bab45fa4b
reyt0m <reytom1715@gmail.com>
2022-07-01 12:20:13 +0900

    update

--- components/blogList/index.js
+++ components/blogList/index.js
@@ -1,7 +1,9 @@
-import React,{useState, useEffect} from "react";
-import Router, {useRouter } from "next/router"
+import React,{useState, useEffect} from 'react';
+import Router, {useRouter } from 'next/router'
+import './styles.module.scss'
+import styles from '../../styles/Home.module.css'
+import Link from "next/link";
 
-import "./styles.scss"
 
 const BlogList = ({blogData}) => {
 	const [blogs, setBlogs] = useState([])
@@ -23,32 +25,32 @@ const BlogList = ({blogData}) => {
 
 	// Router event handler
 	useEffect(() => {
-		Router.events.on("routeChangeStart", startLoading)
-		Router.events.on("routeChangeComplete", stopLoading)
+		Router.events.on('routeChangeStart', startLoading)
+		Router.events.on('routeChangeComplete', stopLoading)
 		return () => {
-			Router.events.off("routeChangeStart", startLoading)
-			Router.events.off("routeChangeComplete", stopLoading)
+			Router.events.off('routeChangeStart', startLoading)
+			Router.events.off('routeChangeComplete', stopLoading)
 		}
 	}, [])
 
 	// Listen to scroll position for loading more data on scroll
 	useEffect(() => {
-		window.addEventListener("scroll", handleScroll);
+		window.addEventListener('scroll', handleScroll);
 		return () => {
-			window.removeEventListener("scroll", handleScroll);
+			window.removeEventListener('scroll', handleScroll);
 			}
 	})
 
 	const handleScroll = () => {
 		// to get page offset of last blog
-		const lastBlog = document.querySelector(".blog-list > blog:last-child");
+		const lastBlogLoaded = document.querySelector('.blog-list > blog:last-child');
 		if (lastBlogLoaded) {
 			const lastBlogLoadedOffset = lastBlog.offsetTop + lastBlog.clientHeight;
 			const pageOffset = window.pageYOffset + window.innerHeight;
 			// Detect when user scrolls down till the last blog
 			if (pageOffset > lastBlogLoadedOffset) {
 				// load more data
-				console.log("load more data");
+				console.log('load more data');
 				// Stop loading more data
 				//
 				if (blogData.currentPage < blogData.totalPages && !loading) {
@@ -67,6 +69,16 @@ const BlogList = ({blogData}) => {
 	return (
 		<div className="blog-list">
 			<ui className="blog-list__list">
+				<main className={styles.main}>
+				<div className={styles.grid}>
+					<div className={styles.card }>
+					<Link href="/content_detail"  >
+						<h2> Documentation  </h2>
+					</Link>{" "}
+					<p> Find in -depth information about Next.js features and API. </p>{" "}
+					</div>
+				</div>{" "}
+				</main>{" "}
 				{blogs.length > 0 && blogs.map((blog, index) => {
 					return (
 						<li className="blog-list__item" key={index}>
