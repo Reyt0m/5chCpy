ec3eaf3c6694b87c9dd85bac651bab668d577f9b
reytom <reytom1715@gmail.com>
2022-09-30 11:56:00 +0900

    fixCss

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,10 @@
-import footer from "./footer";
+import footer from "./footer.module.scss";
 import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 const Footer = ({}) => {
   return (
     <>
-      <div className={`py-3 bg-light`}>
+      <div className={` bg-light`}>
         <Container>
           <p className={`m-0 text-center text-dark ${footer.content}`}>
             ５ちゃんねるのご利用は利用者各位のご判断にお任せしています｜
